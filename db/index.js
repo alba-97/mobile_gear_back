2f3b51846904f2e55a66ba10ce66d8aab2de05e0
alba-97 <alvarezbasilio97@gmail.com>
2023-07-06 23:23:32 -0300

    prueba

--- db/index.js
+++ db/index.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 const Sequelize = require("sequelize");
 
-const db = new Sequelize(process.env.DB_URL, { logging: false });
+const db = new Sequelize(process.env.DB_URL);
 
 module.exports = db;
