c763b97cb3fee66a4a4c9f8fae2584d2cbafaa7c
alba-97 <alvarezbasilio97@gmail.com>
2023-07-07 14:27:21 -0300

    fix

--- db/index.js
+++ db/index.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const Sequelize = require("sequelize");
 
-const db = new Sequelize(DB_URL, {
+const db = new Sequelize(process.env.DB_URL, {
   dialectModule: require("pg"),
   logging: false,
 });
